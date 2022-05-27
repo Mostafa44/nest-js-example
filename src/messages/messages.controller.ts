89316badcb24c6140a562e398abc5d7ed2b128e2
mostafa.abdelshafy <mostafa.abdelshafy88@gmail.com>
2022-05-27 21:16:25 +0200

    resolve the issue of not finding a message by a given id

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,5 +1,5 @@
 import { MessagesService } from './messages.service';
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, NotFoundException, Param, Post } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
 
 
@@ -22,8 +22,13 @@ export class MessagesController {
             return this.messagesService.create(body.content);
     }
     @Get('/:id')
-    getMessage(@Param('id') id: string){
-        return this.messagesService.findOne(id);
+    async getMessage(@Param('id') id: string){
+        const message= await this.messagesService.findOne(id);
+        if(!message){
+            throw new NotFoundException("message was not found!");
+        }
+
+        return message;
 
     }
 }
